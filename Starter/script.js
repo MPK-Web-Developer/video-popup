e48d97dfbfb0bcc35f15e391117d6b3ba93eb596
praveenWebDesigner360 <m.praveenkumar@gmail.com>
2023-05-29 19:55:35 +0530

    layout created & Popup

---
+++ Starter/script.js
@@ -0,0 +1,15 @@
+function toggleButton() {
+	let video_fix_div = document.querySelector(".video-popup-container");
+	video_fix_div.classList.toggle("active");
+	let video = document.querySelector(".video-popup-iframe");
+	// this script is not working in a youtube popup video
+	video.pause();
+	video.currentTime = 0;
+}
+// let's write 3 line jquery code for stop background music
+$(function () {
+	// #close and iframe direct call from html file no need to store any variable
+	$("#close").click(function () {
+		$("iframe").attr("src", $("iframe").attr("src"));
+	});
+});
